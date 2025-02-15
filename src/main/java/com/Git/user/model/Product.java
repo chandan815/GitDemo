7bf86a0bbb790230eb5cbdafb991aadd00e38266
chandan815 <chandankumarshettyy0304@gmail.com>
2025-02-15 16:56:01 +0530

    adding product entity

---
+++ src/main/java/com/Git/user/model/Product.java
@@ -0,0 +1,34 @@
+package com.Git.user.model;
+
+public class Product {
+    private int productId;
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    private String productName;
+    private  int price;
+
+
+}
